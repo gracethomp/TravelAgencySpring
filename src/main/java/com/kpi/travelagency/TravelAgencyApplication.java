843b90cb268e1c3dd034bb8bcc6e75a0e616abf2
Olena Babii <gracetomlinson26@gmail.com>
2022-12-23 22:03:10 +0200

    start of web part

--- src/main/java/com/kpi/travelagency/TravelAgencyApplication.java
+++ src/main/java/com/kpi/travelagency/TravelAgencyApplication.java
@@ -2,12 +2,13 @@ package com.kpi.travelagency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TravelAgencyApplication {
+public class TravelAgencyApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(TravelAgencyApplication.class, args);
     }
-
 }
--- src/main/java/com/kpi/travelagency/repo/TourRepository.java
+++ src/main/java/com/kpi/travelagency/repo/TourRepository.java
@@ -2,6 +2,6 @@ package com.kpi.travelagency.repo;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface TourRepository extends CrudRepository {
+public interface TourRepository {
 
 }
