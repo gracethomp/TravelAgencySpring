44e1860fe818fbc1b5d6b320aa38c2f49f9c65db
Yelyzaveta Ahapova <laggapova@gmail.com>
2022-12-26 00:33:51 +0100

    Merge pull request#2 from Ahapova
    Frontend user/manager profile, edit/log in page + contollers

---
+++ src/main/java/com/kpi/travelagency/contollers/ManagerProfileController.java
@@ -0,0 +1,20 @@
+package com.kpi.travelagency.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class ManagerProfileController {
+    @GetMapping("/managerProfile")
+    public String managerProfile(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> model
+    ) {
+
+        model.put("name", name);
+        return "managerProfile";
+    }
+}
