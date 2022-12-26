2142752e3b943d2625260c96bbd728522669a379
Yelyzaveta Ahapova <laggapova@gmail.com>
2022-12-26 15:11:23 +0100

    Filled enums Role and Permission

--- src/main/java/com/kpi/travelagency/constants/Permission.java
+++ src/main/java/com/kpi/travelagency/constants/Permission.java
@@ -1,4 +1,25 @@
 package com.kpi.travelagency.constants;
 
 public enum Permission {
+    CREATE_ORDER("Create order"),
+    MANAGE_ORDER("Manage order"),
+    CANCEL_ORDER("Cancel order"),
+
+    MANAGE_TOUR("Manage tour"),
+    DELETE_TOUR("Delete tour"),
+    VIEW_STATISTICS("View statistics"),
+    MANAGE_USERS("Manage users"),
+
+    DELETE_USERS("Delete user"),
+    GRANT_ROLES("Grant roles");
+
+    private final String name;
+
+    Permission(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
