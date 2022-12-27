81f536993bc1a09ac58e7ac053820f2e3397faa6
BorysenkoLiza <90791893+BorysenkoLiza@users.noreply.github.com>
2022-12-27 20:12:27 +0200

    Update TourService.java

--- src/main/java/com/kpi/travelagency/service/TourService.java
+++ src/main/java/com/kpi/travelagency/service/TourService.java
@@ -1,15 +1,13 @@
 package com.kpi.travelagency.service;
 
 import com.kpi.travelagency.entity.Tour;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TourService {
-    Tour saveTour(Tour tour);
+    Tour findById(Long id);
+    Tour saveTour(Tour tour) throws Exception;
     List<Tour> findAll();
-    Tour updateTour(Tour tour, Long tourId);
-    void deleteTourById(Long tourId);
-    Optional<Tour> getTourById(Long tourId);
+    void updateTour(Tour tour) throws Exception;
+    void deleteTourById(Long id) throws Exception;
 }
