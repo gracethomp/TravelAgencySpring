d2158c5a98f51f2073aaff4d522004418aa9bf78
BorysenkoLiza <90791893+BorysenkoLiza@users.noreply.github.com>
2022-12-27 20:12:11 +0200

    Update TourServiceImpl.java

--- src/main/java/com/kpi/travelagency/service/TourServiceImpl.java
+++ src/main/java/com/kpi/travelagency/service/TourServiceImpl.java
@@ -2,21 +2,31 @@ package com.kpi.travelagency.service;
 
 import com.kpi.travelagency.entity.Tour;
 import com.kpi.travelagency.repo.TourRepository;
-import com.kpi.travelagency.service.TourService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TourServiceImpl implements TourService {
     @Autowired
     private TourRepository tourRepository;
 
+    private boolean existsById(Long id){
+        return tourRepository.existsById(id);
+    }
+
     @Override
-    public Tour saveTour(Tour tour) {
+    public Tour findById(Long id){
+        return tourRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Tour saveTour(Tour tour) throws Exception{
+        if (tour.getId() != null && existsById(tour.getId())) {
+            throw new Exception("Tour with id: " + tour.getId() + " already exists");
+        }
         return tourRepository.save(tour);
     }
     @Override
@@ -25,26 +35,30 @@ public class TourServiceImpl implements TourService {
         return tours;
     }
 
-    @Override //need to finish later
-    public Tour updateTour(Tour tour, Long tourId) {
-        return null;
+    @Override
+    public void updateTour(Tour tour) throws Exception {
+        if (!existsById(tour.getId())) {
+            throw new Exception("Cannot find tour with id: " + tour.getId());
+        }
+        tourRepository.save(tour);
     }
 
     @Override
-    public void deleteTourById(Long tourId) {
-        tourRepository.deleteById(tourId);
+    public void deleteTourById(Long id) throws Exception{
+        if (!existsById(id)) {
+            throw new Exception("Cannot find tour with id: " + id);
+        }
+        else {
+            tourRepository.deleteById(id);
+        }
     }
 
-    @Override
-    public Optional<Tour> getTourById(Long tourId){
-        return tourRepository.findById(tourId);
-    }
 
-     /*public List<Tour> findAll(int pageNumber, int rowPerPage) {
-             List<Tour> tours = new ArrayList<>();
-             Pageable sortedByLastUpdateDesc = PageRequest.of(pageNumber - 1, rowPerPage,
-                     Sort.by("id").ascending());
-             tourRepository.findAll(sortedByLastUpdateDesc);
-             return tours;
-         }*/
+    /*public List<Tour> findAll(int pageNumber, int rowPerPage) {
+            List<Tour> tours = new ArrayList<>();
+            Pageable sortedByLastUpdateDesc = PageRequest.of(pageNumber - 1, rowPerPage,
+                    Sort.by("id").ascending());
+            tourRepository.findAll(sortedByLastUpdateDesc);
+            return tours;
+        }*/
 }
