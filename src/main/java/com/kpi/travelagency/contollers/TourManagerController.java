2b12f45fa62e1feec8a4e495cf176981f6a099e2
BorysenkoLiza <90791893+BorysenkoLiza@users.noreply.github.com>
2022-12-27 20:14:21 +0200

    Update TourManagerController.java

--- src/main/java/com/kpi/travelagency/contollers/TourManagerController.java
+++ src/main/java/com/kpi/travelagency/contollers/TourManagerController.java
@@ -1,14 +1,16 @@
 package com.kpi.travelagency.contollers;
 
+import com.kpi.travelagency.entity.Country;
 import com.kpi.travelagency.entity.Tour;
+import com.kpi.travelagency.service.CountryService;
 import com.kpi.travelagency.service.TourService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
@@ -17,8 +19,11 @@ import java.util.Optional;
 
 @Controller
 public class TourManagerController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     TourService tourService;
+
 
     @GetMapping("/toursManager")
     public String tours(Model model) {
@@ -27,30 +32,98 @@ public class TourManagerController {
         model.addAttribute("tours", tours);
         return "toursManager";
     }
-
+    @GetMapping(value = "/toursManager/{id}")
+    public String getUserById(Model model, @PathVariable Long id){
+        Tour tour = null;
+        try {
+            tour = tourService.findById(id);
+            model.addAttribute("allowDelete", false);
+        } catch (Exception ex) {
+            model.addAttribute("errorMessage", ex.getMessage());
+        }
+        model.addAttribute("tour",tour);
+        return "tour";
+    }
     @GetMapping("/toursManager/createTour")
     public String createTour(Model model){
-        model.addAttribute("tour",new Tour());
+        Tour tour = new Tour();
+        model.addAttribute("add",true);
+        model.addAttribute("tour",tour);
         return "createTour";
     }
 
-    @RequestMapping
-    public String saveTour(@ModelAttribute("tour") Tour tour){
-        tourService.saveTour(tour);
-        return "redirect:/";
+    @PostMapping("/toursManager/createTour")
+    public String addTour(Model model,
+                          @ModelAttribute("tour") Tour tour) throws Exception {
+        try {
+            Tour newTour = tourService.saveTour(tour);
+            return "redirect:/toursManager";
+        } catch (Exception ex) {
+            String errorMessage = ex.getMessage();
+            logger.error(errorMessage);
+            model.addAttribute("errorMessage",errorMessage);
+            model.addAttribute("add",true);
+            return "createTour";
+        }
     }
 
-    @RequestMapping("toursManager/editTour/{id}")
-    public ModelAndView showEditTourPage(@PathVariable(name="id") Long id){
-        ModelAndView mav = new ModelAndView("new");
-        Optional<Tour> tour = tourService.getTourById(id);
-        mav.addObject("tour",tour);
-        return mav;
+    @GetMapping(value = {"/toursManager/{id}/editTour"})
+    public String showEditTour(Model model, @PathVariable Long id) {
+        Tour tour = null;
+        try {
+            tour = tourService.findById(id);
+        } catch (Exception ex){
+            model.addAttribute("errorMessage", ex.getMessage());
+        }
+        model.addAttribute("add",false);
+        model.addAttribute("tour",tour);
+        return "createTour";
     }
 
-    @RequestMapping("toursManager/deleteTour/{id}")
-    public String deleteTour(@PathVariable(name="id") Long id){
-        tourService.deleteTourById(id);
-        return "redirect:/";
+    @PostMapping(value = {"/toursManager/{id}/editTour"})
+    public String updateTour(Model model,
+                             @PathVariable Long id,
+                             @ModelAttribute("tour") Tour tour){
+        try{
+            tour.setId(id);
+            tourService.updateTour(tour);
+            return "redirect:/toursManager";
+        } catch (Exception ex){
+            String errorMessage = ex.getMessage();
+            logger.error(errorMessage);
+            model.addAttribute("errorMessage", errorMessage);
+            model.addAttribute("add",false);
+            return "createTour";
+        }
     }
+
+    @GetMapping(value = {"/toursManager/{id}/delete"})
+    public String showDeleteTour(
+            Model model, @PathVariable Long id) {
+        Tour tour = null;
+        try{
+            tour = tourService.findById(id);
+        } catch (Exception ex){
+            model.addAttribute("errorMessage", ex.getMessage());
+        }
+        model.addAttribute("allowDelete",true);
+        model.addAttribute("tour",tour);
+        return "tour";
+    }
+
+    @PostMapping(value = {"/toursManager/{id}/delete"})
+    public String deleteTourById(
+            Model model, @PathVariable Long id) {
+        try {
+            tourService.deleteTourById(id);
+            return "redirect:/toursManager";
+        } catch (Exception ex) {
+            String errorMessage = ex.getMessage();
+            logger.error(errorMessage);
+            model.addAttribute("errorMessage",errorMessage);
+            return "tour";
+        }
+    }
+
+
 }
