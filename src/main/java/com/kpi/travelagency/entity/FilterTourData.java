1aab546121a90767f0e2ea33cce5de5eab0cf3e8
BorysenkoLiza <90791893+BorysenkoLiza@users.noreply.github.com>
2022-12-30 16:04:54 +0200

    altered a little for filtering system

--- src/main/java/com/kpi/travelagency/entity/FilterTourData.java
+++ src/main/java/com/kpi/travelagency/entity/FilterTourData.java
@@ -3,6 +3,7 @@ package com.kpi.travelagency.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kpi.travelagency.constants.TransportType;
 import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -15,12 +16,14 @@ public class FilterTourData {
     private Long id;
     private String country;
     private String city;
-    private Double minPrice;
-    private Double maxPrice;
+    private Double startPrice;
+    private Double endPrice;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "start_date")
     private LocalDate startDate;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "end_date")
     private LocalDate endDate;
 
@@ -30,20 +33,20 @@ public class FilterTourData {
     public FilterTourData() {
         country = null;
         city = null;
-        minPrice = null;
-        maxPrice = null;
+        startPrice = null;
+        endPrice = null;
         startDate = null;
         endDate = null;
         transportType = null;
         hotelRating = null;
     }
 
-    public FilterTourData(Long id, String country, String city, Double minPrice, Double maxPrice, LocalDate startDate, LocalDate endDate, String transportType, Integer hotelRating) {
+    public FilterTourData(Long id, String country, String city,  Double startPrice, Double endPrice, LocalDate startDate, LocalDate endDate, String transportType, Integer hotelRating) {
         this.id = id;
         this.country = country;
         this.city = city;
-        this.minPrice = minPrice;
-        this.maxPrice = maxPrice;
+        this.startPrice = startPrice;
+        this.endPrice = endPrice;
         this.startDate = startDate;
         this.endDate = endDate;
         this.transportType = transportType;
@@ -74,20 +77,20 @@ public class FilterTourData {
         this.transportType = transportType;
     }
 
-    public Double getMinPrice() {
-        return minPrice;
+    public Double getStartPrice() {
+        return startPrice;
     }
 
-    public void setMinPrice(Double minPrice) {
-        this.minPrice = minPrice;
+    public void setStartPrice(Double startPrice) {
+        this.startPrice = startPrice;
     }
 
-    public Double getMaxPrice() {
-        return maxPrice;
+    public Double getEndPrice() {
+        return endPrice;
     }
 
-    public void setMaxPrice(Double maxPrice) {
-        this.maxPrice = maxPrice;
+    public void setEndPrice(Double endPrice) {
+        this.endPrice = endPrice;
     }
 
     public LocalDate getStartDate() {
@@ -122,3 +125,4 @@ public class FilterTourData {
         this.id = id;
     }
 }
+
