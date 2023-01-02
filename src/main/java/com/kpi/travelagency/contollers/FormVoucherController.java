563c71bdcef6c2830c1ca9a196247ed7d0b9e1a9
Yelyzaveta Ahapova <laggapova@gmail.com>
2023-01-02 14:55:22 +0100

    Some mistakes fixed

--- src/main/java/com/kpi/travelagency/contollers/FormVoucherController.java
+++ src/main/java/com/kpi/travelagency/contollers/FormVoucherController.java
@@ -3,6 +3,8 @@ package com.kpi.travelagency.contollers;
 import com.kpi.travelagency.constants.Status;
 import com.kpi.travelagency.entity.*;
 import com.kpi.travelagency.service.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +25,8 @@ public class FormVoucherController {
     private TourServiceImpl tourService;
     @Autowired
     private UserService userService;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @GetMapping("/formVoucher/{id}")
     public String formVoucher(@PathVariable Integer id, Model model) {
         Tour tour = tourService.findById(Long.valueOf(id));
@@ -39,18 +43,28 @@ public class FormVoucherController {
         voucher.setId(-1);
         voucherService.save(voucher);
 
-        User user1 = userService.findUserByEmail(email);
-        voucher.setId(voucherService.setRightID());
-        UserNode userNode = new UserNode();
-        userNode.setId(user1.getId());
-        userNodeService.save(userNode);
+        try {
+            User user1 = userService.logIn(email);
+            voucher.setId(voucherService.setRightID());
+            UserNode userNode = new UserNode();
+            userNode.setId(user1.getId());
+            userNodeService.save(userNode);
 
-        voucherService.makeRelationshipUserVoucher(voucher, userNode);
+            voucherService.makeRelationshipUserVoucher(voucher, userNode);
 
-        TourNode tourNode = new TourNode();
-        tourNode.setId(id);
-        tourNodeService.save(tourNode);
-        voucherService.makeRelationshipTourVoucher(voucher, tourNode);
-        return "home";
+            TourNode tourNode = new TourNode();
+            tourNode.setId(id);
+            tourNodeService.save(tourNode);
+            voucherService.makeRelationshipTourVoucher(voucher, tourNode);
+            return "home";
+
+        } catch (Exception ex) {
+            String errorMessage = ex.getMessage();
+            logger.error(errorMessage);
+            model.addAttribute("errorMessage", errorMessage);
+            model.addAttribute("add",true);
+            return "logIn";
+        }
+
     }
 }
--- src/main/java/com/kpi/travelagency/entity/Manager.java
+++ src/main/java/com/kpi/travelagency/entity/Manager.java
@@ -1,4 +1,30 @@
 package com.kpi.travelagency.entity;
 
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
 public class Manager {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    private String name;
+
+    private String email;
+
+    public Manager(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Manager(){
+
+    }
+
+    
 }
